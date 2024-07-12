14b856af2bd15fda81e285006d245bad5bb397c4
sonbkt303 <kimsonbui303@gmail.com>
2024-07-12 14:11:08 +0700

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-console.log('1111111)
+console.log('1111111');
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
