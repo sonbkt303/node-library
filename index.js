9422e492f4b9d4e4b1575c5509247c1ef2010464
mike bui <mike.bui.0031>
2024-08-01 10:23:48 +0700

    update

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+require("dotenv").config();
 
-console.log('1111111');
+
+console.log(process.env.MONGODB_PASSWORD)
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
